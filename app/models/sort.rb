bafe8107e6781327ae971c6863b426db6fde1cd1
mikaelsn <mikael.snellman@cs.helsinki.fi>
2013-10-08 00:53:50 +0300

    starting to implement bubblesort

--- app/models/sort.rb
+++ app/models/sort.rb
@@ -1,2 +1,16 @@
 class Sort < ActiveRecord::Base
+
+	def bubble(keys)
+		bubble!(keys.clone)
+	end
+
+	def bubble!(keys)
+		0.upto(keys.size-1) do |i|
+			(keys.size-1).downto(i+1) do |j|
+				(keys[j], keys[j-1] = keys[j-1], keys[j]) if keys[j] < keys[j-1]
+			end
+		end
+		keys
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Tiralabra::Application.routes.draw do
   resources :sorts
 
+  root to: 'sorts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
