3518005dadf0388cb42cc2a5422cb9b7b525a254
mikaelsn <mikael.snellman@cs.helsinki.fi>
2013-10-08 22:36:16 +0300

    merge sort

--- app/models/sort.rb
+++ app/models/sort.rb
@@ -1,5 +1,5 @@
 class Sort < ActiveRecord::Base
-	
+
 	attr_accessible :bubble, :quick, :merge
 
 	def self.bubble(list)
@@ -18,6 +18,23 @@ class Sort < ActiveRecord::Base
 		pivot = sl.pop
 		left, right = sl.partition { |e| e < pivot }
 		quick_sort(left) + [pivot] + quick_sort(right)
+	end
+
+	def self.merge_sort(list)
+		return list if list.size < 2
+		left = list[0, list.length/2]
+		right = list[list.length/2, list.length]
+
+		merge(merge_sort(left), merge_sort(right))
+	end
+
+	def self.merge(left, right)
+		sorted_list = []
+		until left.empty? || right.empty?
+			sorted_list << (left[0] <= right[0] ? left.shift : right.shift)
+		end
+		sorted_list.concat(left).concat(right)
+		p sorted_list
 	end
 
 end
