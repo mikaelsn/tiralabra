1a69ce84ebe84496f8714e2c31111770177c54dc
mikaelsn <mikael.snellman@cs.helsinki.fi>
2013-10-08 21:53:20 +0300

    added quicksort

--- app/models/sort.rb
+++ app/models/sort.rb
@@ -10,4 +10,12 @@ class Sort < ActiveRecord::Base
 		end
 	end
 
+	def self.quick_sort(list)
+		sl = list.clone
+		return sl if sl.size <= 1
+		pivot = sl.pop
+		left, right = sl.partition { |e| e < pivot }
+		quick_sort(left) + [pivot] + quick_sort(right)
+	end
+
 end
