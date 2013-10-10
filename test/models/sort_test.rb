4e1e5d5cbdcb9658d69e54d5bac3be7f2a484a39
mikaelsn <mikael.snellman@cs.helsinki.fi>
2013-10-10 10:34:47 +0300

    minor

--- test/models/sort_test.rb
+++ test/models/sort_test.rb
@@ -4,4 +4,15 @@ class SortTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "Bubblesort normal case" do
+  	#TODO
+  end
+
+  test "Quicksort normal case" do
+  	#TODO
+  end
+
+    test "Mergesort normal case" do
+  	#TODO
+  end
 end
